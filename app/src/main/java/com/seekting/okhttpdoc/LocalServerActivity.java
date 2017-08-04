a396c0952a9c2b257c872a3c639076597d1c1856
zhangxingting <zhangxingting@mobimagic.com>
2017-08-04 17:36:53 +0800

    commit

--- app/src/main/java/com/seekting/okhttpdoc/LocalServerActivity.java
+++ app/src/main/java/com/seekting/okhttpdoc/LocalServerActivity.java
@@ -6,11 +6,18 @@ import android.util.Log;
 
 import java.io.IOException;
 
+import javax.annotation.Nullable;
+
+import okhttp3.Authenticator;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.Route;
+import okio.BufferedSink;
 
 /**
  * Created by seekting on 17-8-3.
@@ -21,9 +28,57 @@ public class LocalServerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.authenticator(new Authenticator() {
+            @Nullable
+            @Override
+            public Request authenticate(Route route, Response response) throws IOException {
+                Log.d("seekting", "LocalServerActivity.authenticate()" + "authenticate" + response.body().string());
+                Request r = response.request();
+                Request.Builder b = r.newBuilder();
+                b.post(new RequestBody() {
+                    @Nullable
+                    @Override
+                    public MediaType contentType() {
+                        return MediaType.parse("text");
+                    }
+
+                    @Override
+                    public void writeTo(BufferedSink sink) throws IOException {
+
+                        sink.write("ok".getBytes());
+                    }
+
+                    @Override
+                    public long contentLength() throws IOException {
+                        return "ok".getBytes().length;
+                    }
+                });
+                return b.build();
+            }
+        });
         OkHttpClient okHttpClient = builder.build();
         Request.Builder requestBuilder = new Request.Builder();
-        requestBuilder.url("http://192.168.1.108:8080");
+        requestBuilder.url("http://192.168.1.108:8080/401");
+        final byte[] bytes = "over".getBytes();
+        requestBuilder.post(new RequestBody() {
+            @Nullable
+            @Override
+            public MediaType contentType() {
+                return MediaType.parse("text");
+            }
+
+            @Override
+            public void writeTo(BufferedSink sink) throws IOException {
+                sink.write(bytes);
+
+            }
+
+            @Override
+            public long contentLength() throws IOException {
+                return bytes.length;
+
+            }
+        });
         Call call = okHttpClient.newCall(requestBuilder.build());
         call.enqueue(new Callback() {
             @Override
